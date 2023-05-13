fcf0228dfe68181f5b676a5d3644e227441cbd9b
ilyes <120855231+Ilyes-CH@users.noreply.github.com>
2023-05-13 16:43:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,32 +3,50 @@ const quoteText = document.getElementById('quote');
 const author = document.getElementById('author');
 const tweetBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('quote-btn');
-
+const loader = document.getElementById('loader');
 // show new Quote
+
+
+
+function showLoader() {
+    loader.style.display = 'block';
+    quoteContainer.style.display = 'none';
+  }
+  
+  function hideLoader() {
+    loader.style.display = 'none';
+    quoteContainer.style.display = 'block';
+  }
+
+
 let apiQuotes = [];
-
 function newQuote(){
-      const  quote = apiQuotes[Math.floor(Math.random()* apiQuotes.length)]
-        // console.log(quote)
-
-        if (quote.author == null){
-            author.textContent = 'Unknown';
-        }else{
-            author.textContent = quote.author;
-        }
-
+    showLoader();
+    const  quote = apiQuotes[Math.floor(Math.random()* apiQuotes.length)]
+    // console.log(quote)
     
-
-        if(quoteText.length>120){
-           
-            quoteText.classList.add('long-quote');
-        
-        }
-        quoteText.textContent = quote.text;
+    if (quote.author == null){
+        author.textContent = 'Unknown';
+    }else{
+        author.textContent = quote.author;
     }
     
+    
+    
+    if(quoteText.length>120){
+        
+        quoteText.classList.add('long-quote');
+        
+    }
+    quoteText.textContent = quote.text;
+    hideLoader();
+//     complete();
+}
+
 // get quotes from API
+
 async function getQuotes(){
+    showLoader();
     const apiUrl = 'https://type.fit/api/quotes';
     try{
         
@@ -38,13 +56,15 @@ async function getQuotes(){
         // console.log(apiQuotes[12]);
     }
     catch(error){
-        // catch the error here
+        console.log(error,' 404');
         
     }
+    hideLoader()
 }
 
 // tweet quote
 function tweetQuote() {
+    
     const tweetUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${author.textContent}`;
     window.open(tweetUrl, '_blank');
 }
@@ -53,9 +73,11 @@ function tweetQuote() {
 newQuoteBtn.addEventListener('click', newQuote);
 tweetBtn.addEventListener('click', tweetQuote);
 
-// on load
-getQuotes();
 
+
+// on load
+
+getQuotes();
 
 
 
